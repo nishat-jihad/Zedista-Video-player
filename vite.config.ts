fdbbd934156fd7d8b8cae2b2dd1effe0a499c377
Nishat Alam <alamnishat456@gmail.com>
2026-06-24 12:38:40 +0600

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Zedista-Video-player/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
